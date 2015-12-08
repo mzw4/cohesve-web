be3a3430bf5fb6c8818f69fbfb4abff24905454d
Vivek Sudarsan <viveksudars@gmail.com>
2015-12-08 14:38:47 -0500

    test

--- routes/events.js
+++ routes/events.js
@@ -16,6 +16,7 @@ router.get('/eventlist', function(req, res) {
  * POST to adduser.
  */
 router.post('/addevent', function(req, res) {
+    console.log('this ran')
     var db = req.db;
     var collection = db.get('eventlist');
     
